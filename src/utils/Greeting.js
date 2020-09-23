40ca6774a0dbb6a187c1614cf50a2d475a9ac97d
Donovan Palmer <donovanpalmer061@gmail.com>
2020-09-23 17:34:43 -0400

    adding inline styles with the Style Property

--- src/utils/Greeting.js
+++ src/utils/Greeting.js
@@ -8,17 +8,26 @@ function Greeting() {
     const hours = date.getHours()
     let timeOfDay
 
-    if (hours < 12) {
-        timeOfDay = "morning"
-    } else if (hours >= 12 && hours <17) {
-        timeOfDay = "afternoon"
-    } else {
-        timeOfDay = "night"
+    const styles = {
+        backgroundColor: "gray",
+        fontSize: "30px"
     }
 
+    if (hours < 12) {
+        timeOfDay = "morning"
+        styles.color = "yellow"
+    } else if (hours >= 12 && hours <17) {
+        timeOfDay = "afternoon"
+        styles.color = "orange"
+    } else {
+        timeOfDay = "night"
+        styles.color = "purple"
+    }
+
+  
     return(
         <div>
-            <h1>My name is {`${firstName} ${lastName}`}</h1>
+            <h1 style={styles}>My name is {`${firstName} ${lastName}`}</h1>
             <h2>Good {timeOfDay}!</h2>
         </div>
     )
