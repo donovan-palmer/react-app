ce717bda6aa63395c1facc4a42d1805edde3f7d9
Donovan Palmer <donovanpalmer061@gmail.com>
2020-09-23 16:04:56 -0400

    learned about moving components to seperate folder

---
+++ src/components/MyInfo.js
@@ -0,0 +1,17 @@
+import React from "react"
+
+function MyInfo() {
+    return (
+      <div>
+        <h1>Donovan Palmer</h1>
+        <p>Im learning react</p>
+        <ul>
+          <li>California</li>
+          <li>Japan</li>
+          <li>Nantucket</li>
+        </ul>
+      </div>  
+  )
+  }
+
+  export default MyInfo
--- src/index.js
+++ src/index.js
@@ -1,23 +1,6 @@
 import React from "react"
 import ReactDOM from "react-dom"
 
-function MyInfo() {
-  return (
-    <div>
-      <h1>Donovan Palmer</h1>
-      <p>Im learning react</p>
-      <ul>
-        <li>California</li>
-        <li>Japan</li>
-        <li>Nantucket</li>
-      </ul>
-    </div>  
-)
-}
+import MyInfo from "./components/MyInfo"
 
-
-
-ReactDOM.render(
-  <MyInfo/>,
-  document.getElementById("root")
-)
+ReactDOM.render(<MyInfo/>,document.getElementById("root"))
